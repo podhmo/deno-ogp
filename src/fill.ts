ec1f68a7a7cc5426378ae715909c18a8fce0ac70
podhmo <ababjam61+github@gmail.com>
2024-12-15 05:18:30 +0900

    feat: support amazon fix #1

--- src/fill.ts
+++ src/fill.ts
@@ -6,6 +6,37 @@ export function fill(ogp: OGP, options: { url?: string }): FullOGP {
     return ogp;
   }
 
+  if (options.url !== undefined) {
+    const url = options.url;
+    // for Amazon product page
+    if (url.startsWith("https://www.amazon.") && url.includes("/dp/")) {
+      // e.g. https://www.amazon.com/{description}/dp/{asin}/...
+      const u = new URL(url);
+      const parts = u.pathname.split("/dp/");
+
+      const asin = parts[1].split("/")[0];
+      const description = decodeURIComponent(parts[0].substring(1)).replaceAll(
+        "-",
+        " ",
+      );
+      if (ogp.ogSiteName === undefined) {
+        ogp.ogSiteName = u.hostname;
+      }
+      if (ogp.ogType === undefined) {
+        ogp.ogType = "website";
+      }
+      if (ogp.ogTitle === undefined) {
+        ogp.ogTitle = `${u.hostname} -- ${description}`;
+      }
+      if (ogp.ogUrl === undefined) {
+        ogp.ogUrl = `${u.origin}/dp/${asin}`;
+      }
+      if (ogp.ogDescription === undefined) {
+        ogp.ogDescription = description;
+      }
+    }
+  }
+
   return {
     ...ogp,
     $kind: "full",
